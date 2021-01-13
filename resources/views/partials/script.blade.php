3cae9e763f8b6aaaf2f01c86f4abcff6a0c1ca24
minhazulmin <minhazul234@gmail.com>
2021-01-13 16:13:46 +0600

    chech all data

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,7 +1,7 @@
 
 
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('/js/app.js') }}"></script>
+<script src="https://rmssearch.herokuapp.com/js/bootstrap.min.js"></script>
+<script src="https://rmssearch.herokuapp.com/js/app.js"></script>
 <script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-2.2.0.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
